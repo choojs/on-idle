70047d6443ad14b84c5b2ebcc803c8e552193ee8
Yoshua Wuyts <yoshuawuyts@gmail.com>
2017-05-30 19:09:17 +0200

    shorter

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ var assert = require('assert')
 var dftOpts = {}
 var log = nanologger('on-idle')
 var hasWindow = typeof window !== 'undefined'
-var hasPerf = hasWindow && typeof window.performance.mark !== 'undefined'
-var hasIdle = hasWindow && typeof window.requestIdleCallback !== 'undefined'
+var perf = hasWindow && window.performance
+var hasPerf = hasWindow && perf.mark
+var hasIdle = hasWindow && window.requestIdleCallback
 
 module.exports = onIdle
 
@@ -24,24 +25,24 @@ function onIdle (cb, opts) {
   }
 
   function handler () {
-    var uuid = (window.performance.now() * 100).toFixed()
+    var uuid = (perf.now() * 100).toFixed()
     var startName = 'start-' + uuid + '-on-idle'
     var endName = 'end-' + uuid + '-on-idle'
     var measureName = 'on-idle [' + uuid + ']'
 
-    window.performance.mark(startName)
+    perf.mark(startName)
     cb()
-    window.performance.mark(endName)
-    window.performance.measure(measureName, startName, endName)
+    perf.mark(endName)
+    perf.measure(measureName, startName, endName)
 
-    var entry = window.performance.getEntriesByName(measureName)[0]
+    var entry = perf.getEntriesByName(measureName)[0]
     var duration = entry.duration.toFixed()
     var msg = 'idle callback execution took ' + duration + 'ms'
     if (duration >= 50) log.warn(msg)
     else log.debug(msg)
 
-    window.performance.clearMeasures(measureName)
-    window.performance.clearMarks(startName)
-    window.performance.clearMarks(endName)
+    perf.clearMeasures(measureName)
+    perf.clearMarks(startName)
+    perf.clearMarks(endName)
   }
 }
